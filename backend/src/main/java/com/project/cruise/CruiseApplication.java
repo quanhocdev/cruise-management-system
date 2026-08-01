df982345a200b41a3044a05c3efda80f9fd95277
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-08-01 12:09:43 +0700

    Add project Spring Backend

---
+++ backend/src/main/java/com/project/cruise/CruiseApplication.java
@@ -0,0 +1,13 @@
+package com.project.cruise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/cruise/CruiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cruise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
