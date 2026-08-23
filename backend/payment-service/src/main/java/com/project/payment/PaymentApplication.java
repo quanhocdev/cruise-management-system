4562c58b326493a853840614472986b93c581fcb
ASUS <zatdat02@gmail.com>
2026-08-23 18:41:52 +0700

    sửa(vận hành): khởi động ổn định các dịch vụ thanh toán

--- backend/payment-service/src/main/java/com/project/payment/PaymentApplication.java
+++ backend/payment-service/src/main/java/com/project/payment/PaymentApplication.java
@@ -2,12 +2,19 @@ package com.project.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication(scanBasePackages = "com.project")
 public class PaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@Bean
+	RestClient.Builder restClientBuilder() {
+		return RestClient.builder();
 	}
 
 }
