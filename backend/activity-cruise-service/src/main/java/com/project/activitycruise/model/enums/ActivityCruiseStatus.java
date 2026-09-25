e4fe5331530b9b99a04dad381f358ae5423befbd
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-09-25 22:45:30 +0700

    clean

--- backend/activity-cruise-service/src/main/java/com/project/activitycruise/model/enums/ActivityCruiseStatus.java
+++
@@ -1,6 +0,0 @@
-package com.project.activitycruise.model.enums;
-
-public enum ActivityCruiseStatus {
-    ACTIVE,
-    INACTIVE
-}
--- backend/convenience-service/src/main/java/com/project/convenience/model/enums/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package com.project.convenience.model.enums;
-
-public enum ProductStatus {
-    ACTIVE,
-    INACTIVE
-}
